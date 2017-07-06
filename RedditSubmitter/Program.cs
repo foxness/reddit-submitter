ecc20c7ee7824b4f8584c5e40e5b733d840114fd
foxneZz <foxnezz@gmail.com>
2017-07-06 19:22:33 +0300

    added basic authorization

--- RedditSubmitter/Program.cs
+++ RedditSubmitter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace RedditSubmitter
     {
         static void Main(string[] args)
         {
+            Run();
+            Console.ReadLine();
+        }
+
+        static async void Run()
+        {
+            var lines = File.ReadAllLines("secret.txt");
+            var clientId = lines[0];
+            var secret = lines[1];
+            var username = lines[2];
+            var password = lines[3];
+
+            var rs = new RedditSubmitter(username, password, clientId, secret);
+            await rs.Authorize();
         }
     }
 }
---
+++ RedditSubmitter/RedditSubmitter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditSubmitter
+{
+    class RedditSubmitter
+    {
+        private const string ACCESS_TOKEN_URL = "https://www.reddit.com/api/v1/access_token";
+
+        private readonly string username;
+        private readonly string password;
+        private readonly string clientId;
+        private readonly string secret;
+
+        private HttpClient client;
+
+        public RedditSubmitter(string username, string password, string clientId, string secret)
+        {
+            this.username = username;
+            this.password = password;
+            this.clientId = clientId;
+            this.secret = secret;
+
+            client = new HttpClient();
+            var byteArray = Encoding.ASCII.GetBytes($"{clientId}:{secret}");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+
+        public async Task Authorize()
+        {
+            var response = await client.PostAsync(ACCESS_TOKEN_URL, new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "grant_type", "password" },
+                { "username", username },
+                { "password", password }
+            }));
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            /*if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response);
+            }*/
+
+            Console.WriteLine(response);
+        }
+    }
+}
